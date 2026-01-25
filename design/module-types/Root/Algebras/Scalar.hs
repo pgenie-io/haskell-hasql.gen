e3c461e73d27b99034c58db566c24472e4abba2c
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-25 22:54:26 +0300

    Sketch the module types design

---
+++ design/module-types/Root/Algebras/Scalar.hs
@@ -0,0 +1,23 @@
+module Root.Algebras.Scalar where
+
+import Data.Tagged (Tagged(..), retag, untag)
+import qualified Hasql.Encoders as Encoders
+import qualified Hasql.Decoders as Decoders
+
+class IsScalar a where
+  encoder :: Encoders.Value a
+  decoder :: Decoders.Value a
+
+instance IsScalar Int where
+  encoder = fromIntegral >$< Encoders.int8
+  decoder = fromIntegral <$> Decoders.int8
+
+instance IsScalar Bool where
+  encoder = Encoders.bool
+  decoder = Decoders.bool
+
+instance IsScalar Text where
+  encoder = Encoders.text
+  decoder = Decoders.text
+
+
